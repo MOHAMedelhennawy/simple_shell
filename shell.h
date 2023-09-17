2c3d637262f2d0b4a7868d10791339db22677b9d
Kareem1715 <Kareem.H1715@gmail.com>
2023-09-17 20:09:33 +0300

    Header file :)

--- shell.h
+++ shell.h
@@ -23,9 +23,17 @@ void free_list(d_node *head);
 
 void getline_error(int bytes, char **buff);
 void env_function();
-void execuve_command_with_slash(char **bffer/*, char *programName, int n*/);
+void execuve_command_with_slash(char **bffer, char *programName, int n);
 
 void error_message(char *argv, char *buff, int num);
+void error_message_permission_denied(char *argv, char *buff, int num);
+
 void print_number(int n);
+
+void _strcpy(char *dest, char *src);
+char *_strdup(char *str);
+int _strlen(char *s);
+void concatenate_string(char* s, char* s1);
+int check_node_for_execuve(d_node **head, char **buffer);
 
 #endif
