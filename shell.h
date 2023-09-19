4b7cb39d9594c997ec64139e7b370f9fa12fcdb5
Kareem1715 <Kareem.H1715@gmail.com>
2023-09-19 20:45:02 +0300

    Simple shell

--- shell.h
+++ shell.h
@@ -5,38 +5,48 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <wait.h>
-#include <sys/stat.h>
 
 extern char **environ;
 
-typedef struct dirNode 
+/**
+ * struct dirNode - build linked list
+ *
+ * @directory: The directory in PATH environment varibale
+ * @nextDir: Points to the next node
+ *
+ * Description: This struct for build a new linked list
+ * to use it in storing PATH directories.
+ */
+typedef struct dirNode
 {
-    char *directory;
-    struct dirNode *nextDir;
-}d_node;
+	char *directory;
+	struct dirNode *nextDir;
+} d_node;
 
-char *_getenv(char *name);
-d_node *build_linkedList(void);
-int search_in_path(char *buf);
-void free_list(d_node *head);
 
 void getline_error(int bytes, char **buff, size_t *ex);
 void env_function(void);
-int execuve_command_with_slash(char **bffer, char *programName, int n, char **pathbuf);
+int execuve_command_with_slash(char **bffer, char *programName, int n,
+char **pathbuf);
+
+char *_getenv(char *name);
+d_node *build_linkedList(void);
+void free_list(d_node *head);
+int search_in_path(char *buf);
+int check_node_for_execuve(d_node **head, char **buffer);
 
 void error_message(char *argv, char *buff, int num);
 void error_message_permission_denied(char *argv, char *buff, int num);
-
 void print_number(int n);
 int exit_with_arguments(char *bufff, char **oldBuf);
 int _atoi(char *s);
 
-void _strcpy(char *dest, char *src);
-char *_strdup(char *str);
-int _strlen(char *s);
-void _strcat(char* s, char* s1);
+void _strcpy(char *firstString, char *secondString);
+char *_strdup(char *string);
+int _strlen(char *string);
+void _strcat(char *stringPart1, char *stringPart2);
 int _strcmp(char *strin1, char *strin2);
-int check_node_for_execuve(d_node **head, char **buffer);
-char *_strtok(char *str, char *delim);
+
+char *_strtok(char *strin, char *delim);
 
 #endif
