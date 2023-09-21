cf6a37f3bfb070193466e179b364c9bc4531b41c
Kareem1715 <Kareem.H1715@gmail.com>
2023-09-22 02:14:31 +0300

    ok

--- print_line_number.c
+++ print_line_number.c
@@ -10,5 +10,5 @@ void print_number(int n)
 	if (n / 10)
 		print_number(n / 10);			/*print number using recursion*/
 	c = (n % 10) + '0';
-	write(STDOUT_FILENO, &c, 1);
+	write(STDERR_FILENO, &c, 1);
 }
