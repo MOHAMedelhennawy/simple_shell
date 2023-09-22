d693b06b32be083e2887f83d8be50d3efbcc5ca0
Mohamed elhennawy <el7ennawy2002@gmail.com>
2023-09-22 19:58:10 +0300

    Fix

--- error_msg_slash.c
+++ error_msg_slash.c
@@ -15,7 +15,7 @@ void error_message_permission_denied(char *Prog, char *buff, int errNum)
 {    /* ./hsh: 1: sasklj: not found */
 	write(STDERR_FILENO, Prog, _strlen(Prog));
 	write(STDERR_FILENO, ": ", 2);
-	print_number(errNum); /* Print line number */
+	print_number(errNum);  /* Print line number */
 	write(STDERR_FILENO, ": ", 2);
 	write(STDERR_FILENO, buff, _strlen(buff));
 	write(STDERR_FILENO, ": Permission denied\n", 20);
